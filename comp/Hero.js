9c6d44eb4173a512cab5ce732f9c513519598913
muhammadabrar <42229174+muhammadabrar@users.noreply.github.com>
2022-11-05 10:47:01 +0500

    checknow

--- comp/Hero.js
+++ comp/Hero.js
@@ -1,19 +1,44 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import css from "../styles/Hero.module.css";
-import map from "../public/map.png";
+import map from "../public/l1.jpg";
+import l2 from "../public/l2.jpg";
+import l3 from "../public/pic (1).jpg";
+
+
 import { Container, Button } from "@mui/material";
 import Link from "next/link";
 const Hero = () => {
+  const [silde, setsilde] = useState(1);
+  useEffect(() => {
+    setTimeout(() => {
+      setsilde(silde == 1 ? 2 : silde == 2 ? 3 : 1);
+    }, 10000);
+  }, [silde]);
   return (
     <div className={css.hero}>
-      <div className={css.mapImage}>
-        <Image src={map} layout="responsive" />
+                    {silde == 1? 
+                     <div className={css.mapImage} >
+                     <Image src={l2} layout="responsive" data-aos={"fade-in"}/>
+                   </div>:
+                   silde == 2?
+                   <div className={css.mapImage} >
+                   <Image src={l3} layout="responsive" data-aos={"fade-in"}/>
+                 </div>:
+                  <div className={css.mapImage} >
+                  <Image src={map} layout="responsive" data-aos={"fade-in"}/>
+                </div>
+                  }
+      {/* <div className={css.mapImage} >
+        <Image src={map} layout="responsive" data-aos={"fade-in"}/>
+      </div> */}
+      <div className={css.overlay}>
+
       </div>
       <div className={css.HeroContent}>
         <Container>
           <>
-            <h3 className={css.cominSoon}>Coming Soon!</h3>
+            <h3 className={css.cominSoon}>Coming Soon! </h3>
             <h1 className={css.Herotext}>
               {" "}
               Get early access to <br />
